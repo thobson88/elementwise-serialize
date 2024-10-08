7c2ecfae84e64bc5eed3cbd09a958efba3a3817a
Timothy Hobson <thobson@turing.ac.uk>
2024-10-08 23:06:23 +0100

    Implement elementwise_deserialize

--- elementwise_serialize_derive/src/lib.rs
+++ elementwise_serialize_derive/src/lib.rs
@@ -1,5 +1,5 @@
 use proc_macro::TokenStream;
-use syn::{Data, DataStruct, Fields};
+use syn::{punctuated::Punctuated, token::Comma, Data, DataStruct, DeriveInput, Field, Fields};
 #[macro_use]
 extern crate quote;
 
@@ -12,17 +12,9 @@ pub fn elementwise_serialize_derive(input: TokenStream) -> TokenStream {
     impl_elementwise_serialize(&ast)
 }
 
-fn impl_elementwise_serialize(ast: &syn::DeriveInput) -> TokenStream {
+fn impl_elementwise_serialize(ast: &DeriveInput) -> TokenStream {
     let name = &ast.ident;
-    // Access the struct fields.
-    // See https://github.com/dtolnay/syn/issues/516#issuecomment-431637005
-    let fields = match &ast.data {
-        Data::Struct(DataStruct {
-            fields: Fields::Named(fields),
-            ..
-        }) => &fields.named,
-        _ => panic!("Expected a struct with named fields"),
-    };
+    let fields = fields(&ast);
     let field_name_iter = fields.iter().map(|field| &field.ident);
 
     // Note: field types are accessible but are not useful for determining if an Option type:
@@ -59,4 +51,74 @@ fn impl_elementwise_serialize(ast: &syn::DeriveInput) -> TokenStream {
         }
     };
     gen.into()
+}
+
+#[proc_macro_derive(ElementwiseDeserialize)]
+pub fn elementwise_deserialize_derive(input: TokenStream) -> TokenStream {
+    // Construct a representation of Rust code as a syntax tree.
+    let ast = syn::parse(input).unwrap();
+
+    // Build the trait implementation.
+    impl_elementwise_deserialize(&ast)
+}
+
+fn impl_elementwise_deserialize(ast: &DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let fields = fields(&ast);
+    let field_name_iter = fields.iter().map(|field| &field.ident);
+    let field_type_iter = fields.iter().map(|field| &field.ty);
+    let field_name_iter_clone = field_name_iter.clone();
+
+    let gen = quote! {
+        impl<'a> ElementwiseDeserialize<'a> for #name {
+            fn elementwise_deserialize(path: &std::path::Path) -> Result<Self, std::io::Error> {
+                #(
+                    // Construct the filename from the struct field.
+                    let filename = format!("{}.json", stringify!(#field_name_iter));
+                    let full_path = path.join(&filename);
+
+                    // Missing files should be interpreted as None values in the struct, so first
+                    // deserialize into a String to accommodate both Option and non-Option types.
+                    let field_value_str: String =  match full_path.exists() {
+                        true => {
+                            // If the file is available, deserialize with serde and then serialize to string.
+                            if let Ok(file) = std::fs::File::open(&full_path) {
+                                let reader = std::io::BufReader::new(file);
+                                let #field_name_iter: #field_type_iter = serde_json::from_reader(reader)?;
+                                serde_json::to_string(&#field_name_iter)?
+                            } else {
+                                let msg = format!("File found but failed to open: {:?}", full_path);
+                                return Err(std::io::Error::new(std::io::ErrorKind::Other, msg))
+                            }
+                        },
+                        false => {
+                            // File not file implies None value, for which the serde serialization is `null`.
+                            "null".to_string()
+                        }
+                    };
+                    // Deseserialize the field value string with serde to get the correct type.
+                    let #field_name_iter: #field_type_iter = serde_json::from_str(&field_value_str)?;
+                )*
+                // Construct the struct from the named fields.
+                Ok(#name {
+                    #(
+                        #field_name_iter_clone,
+                    )*
+                })
+            }
+        }
+    };
+    gen.into()
+}
+
+fn fields(ast: &DeriveInput) -> &Punctuated<Field, Comma> {
+    // Access the struct fields.
+    // See https://github.com/dtolnay/syn/issues/516#issuecomment-431637005
+    match &ast.data {
+        Data::Struct(DataStruct {
+            fields: Fields::Named(fields),
+            ..
+        }) => &fields.named,
+        _ => panic!("Expected a struct with named fields"),
+    }
 }
--- use_elementwise_serialize/src/main.rs
+++ use_elementwise_serialize/src/main.rs
@@ -1,17 +1,17 @@
-use elementwise_serialize::ElementwiseSerialize;
-use elementwise_serialize_derive::ElementwiseSerialize;
-use serde::Serialize;
+use elementwise_serialize::{ElementwiseDeserialize, ElementwiseSerialize};
+use elementwise_serialize_derive::{ElementwiseDeserialize, ElementwiseSerialize};
+use serde::{Deserialize, Serialize};
 use serde_with::skip_serializing_none;
 use std::path::PathBuf;
 
-#[derive(Debug, Clone, Serialize, ElementwiseSerialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct RequesterDetails {
     requester_org: String,
     operator_name: String,
 }
 
 #[skip_serializing_none]
-#[derive(Debug, Clone, Serialize, ElementwiseSerialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, ElementwiseSerialize, ElementwiseDeserialize)]
 struct Attestation {
     requester_details: RequesterDetails,
     udid: Option<String>,
@@ -30,6 +30,12 @@ fn main() {
         udid,
         ddid,
     };
+
+    // Serialize to the current directory.
     let path_buf = PathBuf::new();
     let _ = attestation.elementwise_serialize(&path_buf);
+
+    // Deserialize.
+    let deserialized = Attestation::elementwise_deserialize(&path_buf).unwrap();
+    println!("Deserialized struct:\n{:?}", deserialized);
 }
